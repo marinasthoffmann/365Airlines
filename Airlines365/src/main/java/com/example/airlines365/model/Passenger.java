75c1fc367a2c6f12dfd5cfbe36b00be71d2a1f36
marina-hoffmann <marina.hoffmann@aurum.com.br>
2023-05-21 08:03:25 -0300

    feat: load initial data

--- Airlines365/src/main/java/com/example/airlines365/model/Passenger.java
+++ Airlines365/src/main/java/com/example/airlines365/model/Passenger.java
@@ -3,26 +3,21 @@ package com.example.airlines365.model;
 import com.example.airlines365.model.enums.Classification;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Table(name = "PASSAGEIROS")
 public class Passenger {
 
     @Id
     private Long cpf;
     private String nome;
-    private LocalDate birthdate;
-    private Classification classification;
-    private Integer mileage;
-    private String eticket;
-    private String seat;
-    private Boolean checkedBag;
-    private LocalDateTime confirmationTime;
+    private LocalDate dataNascimento;
+    @Enumerated(EnumType.STRING)
+    private Classification classificacao;
+    private Integer milhas;
 }
